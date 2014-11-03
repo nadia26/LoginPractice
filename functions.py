4ea0370403e4cdd2b42e199649f49ca0add90065
Veronika <vazzara1@gmail.com>
2014-11-03 13:19:59 -0500

    some function stuff?

--- functions.py
+++ functions.py
@@ -3,10 +3,18 @@ from pymongo import Connection
 conn = Connection()
 db = conn['people']
 
-def add_user(user, password):
-    d = {'uname': user, 'pword': password}
-    db.people.insert(d)
+def add_user(username, name, password):
+    d = {'username': username,'name':name, 'password': password}
+    if check(username):
+        return "error: username already there"
+    else:
+        db.people.insert(d)
+        return "added user"
     
 
-def authenticate(user, password):
+def authenticate(username, password):
     pass
+#check to see if username & password match, if they do, will return true
+def check(username):
+    pass
+#if the given username exists, it will return true
